832568373235c1d461761ec0f468fb7afcda3a1f
izzy777 <88389421+izzy777@users.noreply.github.com>
2024-08-05 14:44:49 -0400

    Add files via upload

---
+++ CustomerMicroservices/src/main/java/com/example/CustomerMicroservices/CustomerMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustomerMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMicroservicesApplication.class, args);
+	}
+
+}
